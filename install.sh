266aa51ffcd35820e21fb3a034792c6a330d5326
Stephen Rhyne <stephen@throwingboulders.com>
2023-11-27 16:12:44 -0600

    fixes install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ kern=$(uname -s)
 
 # Determine installation binary  
 if [[ "$arch" == "x86_64" && "$kern" == "Linux" ]]; then
-    binary=program-x86_64-linux
+    binary=x86_64-unknown-linux-gnu
 elif [[ "$arch" == "x86_64" && "$kern" == "Darwin" ]]; then
-    binary=program-x86_64-osx 
-elif [[ "$arch" == "aarch64" && "$kern" == "Darwin" ]]; then
-    binary=program-aarch64-osx
+    binary=x86_64-apple-darwin
+elif [[ "$arch" == "arm64" && "$kern" == "Darwin" ]]; then
+    binary=aarch64-apple-darwin
 elif [[ "$arch" == "x86_64" && "$kern" == "MINGW64"* ]]; then 
     binary=program-x86_64-windows
 else
@@ -21,8 +21,8 @@ else
 fi
 
 # Download and install
-curl -sL https://raw.githubusercontent.com/srhyne/doinfo/main/builds/$binary/doinfo -o program
-chmod +x program
-sudo mv program /usr/local/bin
+curl -sL https://raw.githubusercontent.com/srhyne/doinfo/main/builds/$binary/doinfo -o doinfo
+chmod +x doinfo
+sudo mv doinfo /usr/local/bin
 
 echo "Installation complete!"
